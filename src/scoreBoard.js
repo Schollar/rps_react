60fdad90b1c0f4bd3145765620b97fbbb7bfb8fe
Schollar <cschollar22@gmail.com>
2022-04-03 18:31:20 -0600

    Started adding comments to code to help myself in the learning process. Added css to the scoreboard as a test to see how to do it.

--- src/scoreBoard.js
+++ src/scoreBoard.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './scoreBoard.css';
 
-
+// Scoreboard component accepts props from the parent(game compoenent)
+// Then Display the information as h1s in a div
 function ScoreBoard(props) {
 
     return (
-        <div>
+        <div className='scoreboard_container'>
             <h1> Wins: {props.score.wins}</h1>
             <h1> Ties: {props.score.ties}</h1>
             <h1> Losses: {props.score.losses}</h1>
